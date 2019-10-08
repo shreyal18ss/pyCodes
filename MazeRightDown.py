280623be46b0370799426b8cfbffe9b22c44ad0b
shreyal18ss <46070077+shreyal18ss@users.noreply.github.com>
2019-10-08 18:09:25 +0530

    Update MazeRightDown.py

--- MazeRightDown.py
+++ MazeRightDown.py
@@ -1,52 +1,26 @@
+
+  
 M=[[1,8,21,7],
 [19,17,10,20],
 [2,18,23,22],
 [14,25,4,13]]
-
-x=1
-R=len(M)
-C=len(M[0])
-y=[[0,0]]
-i=0
-j=0
-
-while i<4 and j<4:
-      if i==3:
-        a=3
-      else:
-        a=i+1
-      if j==3:
-        b=3
-      else:
-        b=j+1      
+t=M[0][0]
+x,y=0,0
+w=t
+print(t)
+while(x<=3):
+  try:
+    if(M[x+1][y]<M[x][y+1]):
+     x+=1
+    elif(M[x+1][y]>M[x][y+1]):  
+     y+=1
+  except(IndexError):
+    if(x<3):
+      x+=1
+    else:
+      break
+  t=M[x][y]
+  print(t)
+  w+=t
   
-      if(M[a][j]<M[i][b]):
-            y.append([a,j])
-            if(i<3):
-              i=i+1
-            else:
-              i=3
-              j=j+1
-            
-            if(x<M[a][j]):
-              x=M[a][j]
-              print(x)
-              print(y)
-          
-
-      elif(M[a][j]>M[i][b]):
-            y.append([i,b])
-            if(j<3):
-              j=j+1
-            else:
-              j=3
-              i=i+1
-            if(x<M[i][b]):
-              x=M[i][b]
-              print(x)
-              print(y)
-
-
-              
-
-          
+print(w)
